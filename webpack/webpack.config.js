df1a973d0045b1220526b0fcf8a60e3a5c2d6a43
stanislav-halyn <stasgalin@gmail.com>
2020-05-26 15:06:53 +0300

    build: add basic configuration for webpack, typescript and react

---
+++ webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rootDir = path.join(__dirname, '../');
+
+
+module.exports = {
+  entry: path.join(rootDir, './src/index.tsx'),
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  output: {
+    path: path.join(rootDir, './dist'),
+    filename: 'bundle.min.js'
+  },
+
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/, 
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(rootDir, './src/index.html')
+    })
+  ]  
+}
