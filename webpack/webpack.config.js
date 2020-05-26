dcf8fbd099ddbb46a4b6b013aa97754738b4aed0
stanislav-halyn <stasgalin@gmail.com>
2020-05-26 15:10:06 +0300

    build: add a webpack-dev-server dependency

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
     rules: [
       { 
         test: /\.tsx?$/, 
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
       }
     ]
   },
