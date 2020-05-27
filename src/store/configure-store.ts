f51a7ba2311b563e6ec8814a1ff048c3abc3d9d0
stanislav-halyn <stasgalin@gmail.com>
2020-05-27 09:44:21 +0300

    feat: connect redux dev tools

--- src/store/configure-store.ts
+++ src/store/configure-store.ts
@@ -1,12 +1,13 @@
 // Modules
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 // Reducers
 import rootReducer from './reducers';
 
 
 export const configureStore = () => {
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, devToolsEnhancer({}));
 
   return store;
 }
