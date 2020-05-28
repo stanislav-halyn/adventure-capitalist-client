76c5dc67357a17bfa22c3fc50c3a280a45aece02
stanislav-halyn <stasgalin@gmail.com>
2020-05-28 15:27:39 +0300

    feat: format the capital in the user info block

--- src/features/game/components/user-info-block/user-info-block.tsx
+++ src/features/game/components/user-info-block/user-info-block.tsx
@@ -2,23 +2,24 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 
+// Utils
+import { formatLargeNumber } from '../../../../utils/number-format.utils';
+
 // Styles
 import styles from './user-info-block.scss';
 
 
 type UserInfoBlockProps = {
   userCapital: number
-}
+};
 
 
 const UserInfoBlock = ({
   userCapital
-}: UserInfoBlockProps) => {
-  return (
-    <div>
-      Your capital is: ${userCapital}
-    </div>
-  );
-};
+}: UserInfoBlockProps) => (
+  <div styleName="common">
+    <h2>Your capital is: {formatLargeNumber(userCapital)}</h2>
+  </div>
+);
 
 export default CSSModules(UserInfoBlock, styles);
